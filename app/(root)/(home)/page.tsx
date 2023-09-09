ace0e58e3f1f6e899069b0bdada240f42535a30e
adrianhajdin <adrianhajdin00@gmail.com>
2023-09-09 11:59:08 +0200

    ...

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div>
-     
+      
     </div>
   )
 }
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import LeftSidebar from '@/components/shared/LeftSidebar'
+import RightSidebar from '@/components/shared/RightSidebar'
 import Navbar from '@/components/shared/navbar/Navbar'
 import React from 'react'
 
@@ -15,7 +16,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
           </div>
         </section>
 
-        RightSidebar
+        <RightSidebar />
       </div>
 
       Toaster
