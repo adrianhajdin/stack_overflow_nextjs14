94834625e0c89b0ffa555c2eeebc1b80d082f381
adrianhajdin <adrianhajdin00@gmail.com>
2023-09-11 16:22:00 +0200

    ...

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -2,7 +2,8 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
-import { GetTopInteractedTagsParams } from "./shared.types";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
+import Tag from "@/database/tag.model";
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
@@ -18,6 +19,19 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
     // Interaction...
 
     return [ {_id: '1', name: 'tag'}, {_id: '2', name: 'tag2'}]
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+
+    const tags = await Tag.find({});
+
+    return { tags }
   } catch (error) {
     console.log(error);
     throw error;
