cb01bcbbcf31ea3038655f61f8ef0dcb49d5f8a1
adrianhajdin <adrianhajdin00@gmail.com>
2023-09-11 15:13:46 +0200

    ...

---
+++ lib/actions/tag.actions.ts
@@ -0,0 +1,25 @@
+"use server"
+
+import User from "@/database/user.model";
+import { connectToDatabase } from "../mongoose";
+import { GetTopInteractedTagsParams } from "./shared.types";
+
+export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
+  try {
+    connectToDatabase();
+
+    const { userId } = params;
+
+    const user = await User.findById(userId);
+
+    if(!user) throw new Error("User not found");
+
+    // Find interactions for the user and group by tags...
+    // Interaction...
+
+    return [ {_id: '1', name: 'tag'}, {_id: '2', name: 'tag2'}]
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
