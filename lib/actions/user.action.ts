33ef117cbc2a8788d506c24a18f8838ae5e08a6f
adrianhajdin <adrianhajdin00@gmail.com>
2023-09-11 12:46:57 +0200

    ...

---
+++ lib/actions/user.action.ts
@@ -0,0 +1,84 @@
+"use server"
+
+import User from "@/database/user.model";
+import { connectToDatabase } from "../mongoose"
+import { CreateUserParams, DeleteUserParams, UpdateUserParams } from "./shared.types";
+import { revalidatePath } from "next/cache";
+import Question from "@/database/question.model";
+
+export async function getUserById(params: any) {
+  try {
+    connectToDatabase();
+
+    const { userId } = params;
+
+    const user = await User.findOne({ clerkId: userId });
+
+    return user;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function createUser(userData: CreateUserParams) {
+  try {
+    connectToDatabase();
+
+    const newUser = await User.create(userData);
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    connectToDatabase();
+
+    const { clerkId, updateData, path } = params;
+
+    await User.findOneAndUpdate({ clerkId }, updateData, {
+      new: true,
+    });
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function deleteUser(params: DeleteUserParams) {
+  try {
+    connectToDatabase();
+
+    const { clerkId } = params;
+
+    const user = await User.findOneAndDelete({ clerkId });
+
+    if(!user) {
+      throw new Error('User not found');
+    }
+
+    // Delete user from database
+    // and questions, answers, comments, etc.
+
+    // get user question ids
+    // const userQuestionIds = await Question.find({ author: user._id}).distinct('_id');
+
+    // delete user questions
+    await Question.deleteMany({ author: user._id });
+
+    // TODO: delete user answers, comments, etc.
+
+    const deletedUser = await User.findByIdAndDelete(user._id);
+
+    return deletedUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
---
+++ lib/validations.ts
@@ -0,0 +1,7 @@
+import * as z from "zod"
+
+export const QuestionsSchema = z.object({
+  title: z.string().min(5).max(130),
+  explanation: z.string().min(100),
+  tags: z.array(z.string().min(1).max(15)).min(1).max(3),
+})
