b627fe3aa4d6578de1fc5d7d99c46bc250bc9bf4
adrianhajdin <adrianhajdin00@gmail.com>
2023-09-16 18:28:29 +0200

    ...

---
+++ lib/actions/tag.actions.ts
@@ -0,0 +1,80 @@
+"use server"
+
+import User from "@/database/user.model";
+import { connectToDatabase } from "../mongoose";
+import { GetAllTagsParams, GetQuestionsByTagIdParams, GetTopInteractedTagsParams } from "./shared.types";
+import Tag, { ITag } from "@/database/tag.model";
+import Question from "@/database/question.model";
+import { FilterQuery } from "mongoose";
+
+export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
+  try {
+    connectToDatabase();
+
+    const { userId } = params;
+
+    const user = await User.findById(userId);
+
+    if(!user) throw new Error("User not found");
+
+    // Find interactions for the user and group by tags...
+    // Interaction...
+
+    return [ {_id: '1', name: 'tag'}, {_id: '2', name: 'tag2'}]
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+
+    const tags = await Tag.find({});
+
+    return { tags }
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getQuestionsByTagId(params: GetQuestionsByTagIdParams) {
+  try {
+    connectToDatabase();
+
+    const { tagId, page = 1, pageSize = 10, searchQuery } = params;
+
+    const tagFilter: FilterQuery<ITag> = { _id: tagId};
+
+    const tag = await Tag.findOne(tagFilter).populate({
+      path: 'questions',
+      model: Question,
+      match: searchQuery
+        ? { title: { $regex: searchQuery, $options: 'i' }}
+        : {},
+      options: {
+        sort: { createdAt: -1 },
+      },
+      populate: [
+        { path: 'tags', model: Tag, select: "_id name" },
+        { path: 'author', model: User, select: '_id clerkId name picture'}
+      ]
+    })
+
+    if(!tag) {
+      throw new Error('Tag not found');
+    }
+
+    console.log(tag)
+    
+    const questions = tag.questions;
+
+    return { tagTitle: tag.name, questions };
+
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
