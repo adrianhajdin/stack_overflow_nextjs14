2ce9bd918d9ab939a7108db05fab53b6465bd0f2
adrianhajdin <adrianhajdin00@gmail.com>
2023-09-17 19:18:11 +0200

    ...

--- app/(root)/profile/edit/page.tsx
+++ app/(root)/profile/edit/page.tsx
@@ -13,7 +13,7 @@ const Page = async ({ params }: ParamsProps) => {
   return (
     <>
       <h1 className="h1-bold text-dark100_light900">Edit Profile</h1>
-
+      
       <div className="mt-9">
         <Profile 
           clerkId={userId}
--- lib/validations.ts
+++ lib/validations.ts
@@ -9,3 +9,11 @@ export const QuestionsSchema = z.object({
 export const AnswerSchema = z.object({
   answer: z.string().min(100)
 })
+
+export const ProfileSchema = z.object({
+  name: z.string().min(5).max(50),
+  username: z.string().min(5).max(50),
+  bio: z.string().min(10).max(150),
+  portfolioWebsite: z.string().url(),
+  location: z.string().min(5).max(50),
+})
