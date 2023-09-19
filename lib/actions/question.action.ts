ac177f9c70949b5675bac69c62905085ca88371b
adrianhajdin <adrianhajdin00@gmail.com>
2023-09-19 12:26:26 +0200

    ...

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -189,3 +189,18 @@ export async function editQuestion(params: EditQuestionParams) {
     console.log(error);
   }
 }
+
+export async function getHotQuestions() {
+  try {
+    connectToDatabase();
+
+    const hotQuestions = await Question.find({})
+      .sort({ views: -1, upvotes: -1 }) 
+      .limit(5);
+
+      return hotQuestions;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -78,3 +78,20 @@ export async function getQuestionsByTagId(params: GetQuestionsByTagIdParams) {
     throw error;
   }
 }
+
+export async function getTopPopularTags() {
+  try {
+    connectToDatabase();
+
+    const popularTags = await Tag.aggregate([
+      { $project: { name: 1, numberOfQuestions: { $size: "$questions" }}},
+      { $sort: { numberOfQuestions: -1 }}, 
+      { $limit: 5 }
+    ])
+
+    return popularTags;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
