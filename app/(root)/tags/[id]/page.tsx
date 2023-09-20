e1a60dabf48ad58d1f7c5a1ffc8d565ab3cf3075
adrianhajdin <adrianhajdin00@gmail.com>
2023-09-20 13:59:28 +0200

    ...

---
+++ app/(root)/tags/[id]/page.tsx
@@ -0,0 +1,55 @@
+import QuestionCard from '@/components/cards/QuestionCard'
+import NoResult from '@/components/shared/NoResult'
+import LocalSearchbar from '@/components/shared/search/LocalSearchbar'
+import { IQuestion } from '@/database/question.model'
+import { getQuestionsByTagId } from '@/lib/actions/tag.actions'
+import { URLProps } from '@/types'
+
+const Page = async ({ params, searchParams }: URLProps) => {
+  const result = await getQuestionsByTagId({
+    tagId: params.id,
+    page: 1,
+    searchQuery: searchParams.q
+  })
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">{result.tagTitle}</h1> 
+
+      <div className="mt-11 w-full">
+        <LocalSearchbar 
+          route={`/tags/${params.id}`}
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search tag questions"
+          otherClasses="flex-1"
+        />
+      </div>
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {result.questions.length > 0 ?
+          result.questions.map((question: IQuestion) => (
+            <QuestionCard 
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+          : <NoResult 
+            title="There’s no tag question saved to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />}
+      </div>
+    </>
+  )
+}
+
+export default Page
