8485b5944d65c943886f5b2f4750ef39059ab43b
Sujata Gunale <67959015+TidbitsJS@users.noreply.github.com>
2023-10-06 00:17:20 +0530

    fix: parsed content on dark version

--- components/shared/ParseHTML.tsx
+++ components/shared/ParseHTML.tsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
-import Prism from 'prismjs'
-import parse from 'html-react-parser'
+import Prism from "prismjs";
+import parse from "html-react-parser";
 
 import "prismjs/components/prism-python";
 import "prismjs/components/prism-java";
@@ -30,18 +30,18 @@ import "prismjs/plugins/line-numbers/prism-line-numbers.css";
 
 interface Props {
   data: string;
+  otherClasses?: string;
 }
 
-const ParseHTML = ({ data }: Props) => {
+const ParseHTML = ({ data, otherClasses }: Props) => {
   useEffect(() => {
     Prism.highlightAll();
-  }, [])
+  }, []);
 
   return (
-    <div>
+    <div className={`markdown w-full min-w-full ${otherClasses}`}>
       {parse(data)}
     </div>
-  )
-}
-
-export default ParseHTML
+  );
+};
+export default ParseHTML;
