f9506563f8c4b180aec934c9d5bb6ef123f18dff
adrianhajdin <adrianhajdin00@gmail.com>
2023-10-05 14:39:12 +0200

    ...

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -8,6 +8,7 @@ import { formatAndDivideNumber } from "@/lib/utils";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { toast } from "../ui/use-toast";
 
 interface Props {
   type: string;
@@ -39,11 +40,19 @@ const Votes = ({
       questionId: JSON.parse(itemId),
       path: pathname,
     })
+
+    return toast({
+      title: `Question ${!hasSaved ? 'Saved in' : 'Removed from'} your collection`,
+      variant: !hasSaved ? 'default' : 'destructive'
+    })
   }
 
   const handleVote = async (action: string) => {
     if(!userId) {
-      return;
+      return toast({
+        title: 'Please log in',
+        description: 'You must be logged in to perform this action',
+      })
     }
 
     if(action === 'upvote') {
@@ -65,8 +74,10 @@ const Votes = ({
         })
       }
 
-      // todo: show a toast
-      return;
+      return toast({
+        title: `Upvote ${!hasupVoted ? 'Successful' : 'Removed'}`,
+        variant: !hasupVoted ? 'default' : 'destructive'
+      })
     }
 
     if(action === 'downvote') {
@@ -88,7 +99,10 @@ const Votes = ({
         })
       }
 
-      // todo: show a toast
+      return toast({
+        title: `Downvote ${!hasupVoted ? 'Successful' : 'Removed'}`,
+        variant: !hasupVoted ? 'default' : 'destructive'
+      })
       
     }
   }
--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -14,7 +14,7 @@ export async function getQuestions(params: GetQuestionsParams) {
   try {
     connectToDatabase();
 
-    const { searchQuery, filter, page = 1, pageSize = 2 } = params;
+    const { searchQuery, filter, page = 1, pageSize = 10 } = params;
 
     // Calculcate the number of posts to skip based on the page number and page size
     const skipAmount = (page - 1) * pageSize;
