6a7dbae98a5892edfef80a0a2da38b9f4230c303
adrianhajdin <adrianhajdin00@gmail.com>
2023-10-05 14:21:25 +0200

    ...

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -17,8 +17,6 @@ export default async function Home({ searchParams }: SearchParamsProps) {
     page: searchParams.page ? +searchParams.page : 1,
   });
 
-  // Fetch Recommended Questions
-
   return (
     <>
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
