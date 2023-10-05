50550bd574b93079cbc97a9aa6da5e7db5736252
Sujata Gunale <67959015+TidbitsJS@users.noreply.github.com>
2023-10-06 00:25:49 +0530

    fix: global search input background on dark mode

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -85,7 +85,7 @@ const GlobalSearch = () => {
             if(!isOpen) setIsOpen(true);
             if(e.target.value === '' && isOpen) setIsOpen(false);
           }}
-          className="paragraph-regular no-focus placeholder text-dark400_light700 background-light800_darkgradient border-none shadow-none outline-none"
+          className="paragraph-regular no-focus placeholder text-dark400_light700 border-none bg-transparent shadow-none outline-none"
         />
       </div>
       {isOpen && <GlobalResult />}
