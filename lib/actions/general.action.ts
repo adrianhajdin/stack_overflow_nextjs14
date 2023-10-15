8d0e31389f074a88225c8c5aff4802737e900bdd
tush-kaul <110074853+tush-kaul@users.noreply.github.com>
2023-10-15 23:00:41 +0530

    Update general.action.ts
    item.clerkId instead of clerkid

--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -42,7 +42,7 @@ export async function globalSearch(params: SearchParams) {
               : item[searchField],
               type,
               id: type === 'user'
-                ? item.clerkid
+                ? item.clerkId
                 : type==='answer'
                   ? item.question 
                   : item._id
