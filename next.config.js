f8fc3d60ea3ba3f1e2f4f492ed70ccbdc0aec7a6
Sujata Gunale <67959015+TidbitsJS@users.noreply.github.com>
2023-11-02 22:27:38 +0530

    feat: upgrade to next.js 14

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: true,
     mdxRs: true,
     serverComponentsExternalPackages: ['mongoose']
   },
@@ -10,11 +9,11 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: '*'
-      }, 
+      },
       {
         protocol: 'http',
         hostname: '*'
-      }, 
+      },
     ]
   }
 }
