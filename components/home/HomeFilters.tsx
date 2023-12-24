4c96fc913e2763222c032dd8b6250d8f40ce662e
amolgameopedia <barkaleamol@gmail.com>
2023-12-24 19:25:42 +0530

    Fix Tailwind CSS syntax

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -40,8 +40,8 @@ const HomeFilters = () => {
     <div className="mt-10 hidden flex-wrap gap-3 md:flex">
       {HomePageFilters.map((item) => (
         <Button key={item.value} onClick={() => {}}
-          className={`body-me dium rounded-lg px-6 py-3 capitalize shadow-none ${active === item.value 
-          ? 'dark:hover:bg-dark400 bg-primary-100 text-primary-500 hover:bg-primary-100 dark:bg-dark-400 dark:text-primary-500' 
+          className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none ${active === item.value 
+          ? 'dark:hover:bg-dark-400 bg-primary-100 text-primary-500 hover:bg-primary-100 dark:bg-dark-400 dark:text-primary-500' 
           : 'bg-light-800 text-light-500 hover:bg-light-800 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-300'
         }`}
         onClickCapture={() => handleTypeClick(item.value)}
