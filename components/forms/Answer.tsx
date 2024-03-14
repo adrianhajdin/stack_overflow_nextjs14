0d2dc1f1588fe2fd6b154f8de6a379374baba4f5
Alper <alperaydinjs@gmail.com>
2024-03-14 18:00:24 +0300

    Refactor Answer component

--- components/forms/Answer.tsx
+++ components/forms/Answer.tsx
@@ -1,40 +1,47 @@
-"use client"
+"use client";
 
-import { Form, FormControl, FormField, FormItem, FormMessage } from '../ui/form'
-import { useForm } from 'react-hook-form'
-import { AnswerSchema } from '@/lib/validations'
-import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Editor } from '@tinymce/tinymce-react'
-import { useRef, useState } from 'react'
-import { useTheme } from '@/context/ThemeProvider'
-import { Button } from '../ui/button'
-import Image from 'next/image'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "../ui/form";
+import { useForm } from "react-hook-form";
+import { AnswerSchema } from "@/lib/validations";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Editor } from "@tinymce/tinymce-react";
+import { useRef, useState } from "react";
+import { useTheme } from "@/context/ThemeProvider";
+import { Button } from "../ui/button";
+import Image from "next/image";
 
 const Answer = () => {
-  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const { mode } = useTheme();
-  const editorRef = useRef(null)
+  const editorRef = useRef(null);
   const form = useForm<z.infer<typeof AnswerSchema>>({
     resolver: zodResolver(AnswerSchema),
     defaultValues: {
-      answer: ''
-    }
-  })
+      answer: "",
+    },
+  });
 
-  const handleCreateAnswer = () => {
-    
-  }
+  const handleCreateAnswer = () => {};
 
   return (
     <div>
       <div className="flex flex-col justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
-        <h4 className="paragraph-semibold text-dark400_light800">Write your answer here</h4>
+        <h4 className="paragraph-semibold text-dark400_light800">
+          Write your answer here
+        </h4>
 
-        <Button className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 text-primary-500 shadow-none dark:text-primary-500"
-        onClick={() => {}}
+        <Button
+          className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 text-primary-500 shadow-none dark:text-primary-500"
+          onClick={() => {}}
         >
-          <Image 
+          <Image
             src="/assets/icons/stars.svg"
             alt="star"
             width={12}
@@ -56,31 +63,44 @@ const Answer = () => {
             render={({ field }) => (
               <FormItem className="flex w-full flex-col gap-3">
                 <FormControl className="mt-3.5">
-                <Editor
-                  apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
-                  onInit={(evt, editor) => {
-                    // @ts-ignore
-                    editorRef.current = editor
-                  }}
-                  onBlur={field.onBlur}
-                  onEditorChange={(content) => field.onChange(content)}
-                  init={{
-                    height: 350,
-                    menubar: false,
-                    plugins: [
-                      'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview', 'anchor',
-                      'searchreplace', 'visualblocks', 'codesample', 'fullscreen',
-                      'insertdatetime', 'media', 'table'
-                    ],
-                    toolbar: 
-                    'undo redo | ' +
-                    'codesample | bold italic forecolor | alignleft aligncenter |' +
-                    'alignright alignjustify | bullist numlist',
-                    content_style: 'body { font-family:Inter; font-size:16px }',
-                    skin: mode === 'dark' ? 'oxide-dark' : 'oxide',
-                    content_css: mode === 'dark' ? 'dark' : 'light', 
-                  }}
-                />
+                  <Editor
+                    apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                    onInit={(evt, editor) => {
+                      // @ts-ignore
+                      editorRef.current = editor;
+                    }}
+                    onBlur={field.onBlur}
+                    onEditorChange={(content) => field.onChange(content)}
+                    init={{
+                      height: 350,
+                      menubar: false,
+                      plugins: [
+                        "advlist",
+                        "autolink",
+                        "lists",
+                        "link",
+                        "image",
+                        "charmap",
+                        "preview",
+                        "anchor",
+                        "searchreplace",
+                        "visualblocks",
+                        "codesample",
+                        "fullscreen",
+                        "insertdatetime",
+                        "media",
+                        "table",
+                      ],
+                      toolbar:
+                        "undo redo | " +
+                        "codesample | bold italic forecolor | alignleft aligncenter |" +
+                        "alignright alignjustify | bullist numlist",
+                      content_style:
+                        "body { font-family:Inter; font-size:16px }",
+                      skin: mode === "dark" ? "oxide-dark" : "oxide",
+                      content_css: mode === "dark" ? "dark" : "light",
+                    }}
+                  />
                 </FormControl>
                 <FormMessage className="text-red-500" />
               </FormItem>
@@ -88,18 +108,18 @@ const Answer = () => {
           />
 
           <div className="flex justify-end">
-              <Button
-                type="button"
-                className="primary-gradient w-fit text-white"
-                disabled={isSubmitting}
-              >
-                {isSubmitting ? 'Submitting...' : 'Submit'}
-              </Button>
+            <Button
+              type="button"
+              className="primary-gradient w-fit text-white"
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? "Submitting..." : "Submit"}
+            </Button>
           </div>
         </form>
       </Form>
     </div>
-  )
-}
+  );
+};
 
-export default Answer
+export default Answer;
